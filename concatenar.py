f27a261cf6ab5d4398adef40045d83ec6d220cba
Mariel Alejandra Cota <mariel15tw27@gmail.com>
2020-02-20 01:16:23 +0000

    Fusionar archivos funciona, falta crear archivo

---
+++ concatenar.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import lector
+import argparse
+
+def juntar_archivos(archivos):
+    contenidos = []
+    print(archivos)
+    for archivo in archivos:
+        texto = lector.leer_archivo(archivo)
+        print(texto)
+        contenidos.append(texto)
+    return contenidos
+    
+def main(archivos, output):
+    textos = juntar_archivos( archivos )
+    texto_limpio = " ".join(textos)
+    print (texto_limpio)
+
+if __name__== "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--archivo', dest = 'archivo', help = "nombre de archivo", action="append", required = True)
+    parser.add_argument('-o', '--output', dest='output', help="archivos en uno", required=True)
+    args = parser.parse_args()
+    archivo = args.archivo
+    output = args.output
+    main(archivo, output)
